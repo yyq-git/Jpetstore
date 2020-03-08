05b5452b46967fea7a656d4831324b2d0b3134e1
yyq-git <61744672+yyq-git@users.noreply.github.com>
2020-03-08 15:34:27 +0800

    Add files via upload

---
+++ src/org/csu/mypetstore/service/CatelogService.java
@@ -0,0 +1,59 @@
+package org.csu.mypetstore.service;
+
+import org.csu.mypetstore.domain.Category;
+import org.csu.mypetstore.domain.Item;
+import org.csu.mypetstore.domain.Product;
+import org.csu.mypetstore.persistence.CategoryDAO;
+import org.csu.mypetstore.persistence.Impl.CategoryDAOImpl;
+import org.csu.mypetstore.persistence.Impl.ItemDAOImpl;
+import org.csu.mypetstore.persistence.Impl.ProductDAOImpl;
+import org.csu.mypetstore.persistence.ItemDAO;
+import org.csu.mypetstore.persistence.ProductDAO;
+
+import java.util.List;
+
+public class CatelogService {
+
+    private CategoryDAO categoryDAO;
+    private ItemDAO itemDAO;
+    private ProductDAO productDAO;
+
+    public CatelogService(){
+        categoryDAO = new CategoryDAOImpl();
+        productDAO = new ProductDAOImpl();
+        itemDAO = new ItemDAOImpl();
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryDAO.getCategoryList();
+    }
+
+    public Category getCategory(String categoryId) {
+        return categoryDAO.getCategory(categoryId);
+    }
+
+    public Product getProduct(String productId) {
+        return productDAO.getProduct(productId);
+    }
+
+    public List<Product> getProductListByCategory(String categoryId) {
+        return productDAO.getProductListByCategory(categoryId);
+    }
+
+    // TODO enable using more than one keyword
+    public List<Product> searchProductList(String keyword) {
+        return productDAO.searchProductList("%" + keyword.toLowerCase() + "%");
+    }
+
+    public List<Item> getItemListByProduct(String productId) {
+        return itemDAO.getItemListByProduct(productId);
+    }
+
+    public Item getItem(String itemId) {
+        return itemDAO.getItem(itemId);
+    }
+
+    public boolean isItemInStock(String itemId) {
+        return itemDAO.getInventoryQuantity(itemId) > 0;
+    }
+}
